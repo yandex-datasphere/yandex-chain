9c0f2da6b6d1e86799d50a0a2ec3ff5ddea9886a
Dmitri Soshnikov <dmitri@soshnikov.com>
2024-01-25 16:26:47 +0300

    Add tests

--- tests/test_YandexGPT.py
+++ tests/test_YandexGPT.py
@@ -11,6 +11,14 @@ class TestYandexGPT(unittest.TestCase):
         self.assertGreater(YGPT.completionTokens,0)
         self.assertGreater(YGPT.inputTextTokens,0)
 
+    def test_full_model(self):
+        YGPT = YandexLLM(config="tests/config.json",use_lite=False)
+        res = YGPT('Imagine no possessions...')
+        self.assertGreater(len(res), 10)
+        self.assertGreater(YGPT.totalTokens,0)
+        self.assertGreater(YGPT.completionTokens,0)
+        self.assertGreater(YGPT.inputTextTokens,0)
+
     def wrong_auth(self):
         YGPT = YandexLLM(folder_id='xxxxxxx',iam_token='xxxxxxx')
         res = YGPT('Hello, world')
